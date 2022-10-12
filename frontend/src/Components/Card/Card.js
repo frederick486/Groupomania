d265b8b631b261b6ddb38a18875e2fe2c2b21677
frederick486 <frederick.gaittet@orange.fr>
2022-10-12 19:16:39 +0200

    révision de la mise en page

--- frontend/src/Components/Card/Card.js
+++ frontend/src/Components/Card/Card.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Card.css'
+import Canyon from '../../Assets/Canyon.jpg'
 
 export default function Card(props) {
+
   return (
     <div className='card'>
+      <img src={Canyon} alt="Canyon" />
+
       {props.children}
+      
     </div>
   )
 }
