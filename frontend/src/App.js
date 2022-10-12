ef563679e43ac24e2a3c4bc1481b25ba9af7af74
frederick486 <frederick.gaittet@orange.fr>
2022-10-12 17:19:54 +0200

    création base de travail + installation dépendances

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Containers/Home/Home';
+import {Routes, Route} from 'react-router-dom'
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
+import AddArticle from './Containers/AddArticle/AddArticle'
+import Contact from './Containers/Contact/Contact'
+import Article from './Containers/Article/Article' 
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/ecrire' element={<AddArticle />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='/article' element={<Article />}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/Components/Form/Form.js
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import './Form.css'
+import Navbar from '../../Components/Navbar/Navbar'
+
+export default function Form() {
+    const [article, setArticle] = useState({
+        title:"",
+        body:"",
+    });
+
+    const dispatch = useDispatch();
+
+    const handleForm = (e) => {
+        e.preventDefault();
+
+        const newArticle = {
+            title: article.title,
+            body: article.body,
+        };
+
+        dispatch({
+            type: 'ADDARTICLE',
+            payload: article,
+        });
+
+        setArticle({
+            title:"",
+            body:"",
+        })
+    };
+
+    const addNewTitle = (e) => {
+        const newObjState = {...article, title: e.target.value};
+        setArticle(newObjState);
+    };
+
+    const addNewBody = (e) => {
+        const newObjState = {...article, body: e.target.value};
+        setArticle(newObjState);
+    };
+  
+  return (
+    <>  
+    <Navbar />
+        <h1 className="title-form">Ecrivez un article</h1>
+        <form className="container-form" onSubmit={handleForm}>
+            <label htmlFor="title">Titre</label>
+            <input 
+                value={article.title}
+                onInput={addNewTitle}
+                type="text" 
+                id="title" 
+                placeholder="Entrez votre nom"
+                // className='inp-title' 
+            />
+
+            <label htmlFor="article">Votre article</label>
+            <textarea 
+                value={article.body}
+                onInput={addNewBody}
+                id="article" 
+                placeholder="Votre article"
+                // className='inp-body'
+            ></textarea>
+
+            <button>Envoyer l'article</button>
+        </form>      
+    </>
+  );
+}
