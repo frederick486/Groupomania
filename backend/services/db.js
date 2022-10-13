7ee406ca185428f69c7121fe77facbce7dc74666
frederick486 <frederick.gaittet@orange.fr>
2022-10-14 00:19:39 +0200

    basculement sur nouvelle base de donnée

--- backend/services/db.js
+++ backend/services/db.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const password = process.env.PASSWORD;
 const login = process.env.LOGIN;
 
-const uri = `mongodb+srv://${login}:${password}@cluster0.o2hbnjb.mongodb.net/?retryWrites=true&w=majority`
+// const uri = `mongodb+srv://${login}:${password}@cluster0.o2hbnjb.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${login}:${password}@groupomania-cluster.jhzkkyb.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose
     .connect(uri)
